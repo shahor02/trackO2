2bf2311a69ca233ee63c4978d704e51f7f57d57d
shahoian <ruben.shahoyan@cern.ch>
2015-12-17 21:14:05 +0100

    LinkDef for cint

---
+++ trackO2LinkDef.h
@@ -0,0 +1,12 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+//#pragma link C++ class AliAlgAux;
+#pragma link C++ class AliceO2::Base::Track::TrackPar;
+#pragma link C++ class AliceO2::Base::Track::TrackParCov;
+
+
+#endif
